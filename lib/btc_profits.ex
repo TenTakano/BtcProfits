027ce3f80cc0be035db474afc531d3b3511cc2bc
TenTakano <takano20771@gmail.com>
2022-02-21 15:51:01 +0900

    Remove unused function

--- lib/btc_profits.ex
+++ lib/btc_profits.ex
@@ -1,18 +1,3 @@
 defmodule BtcProfits do
-  @moduledoc """
-  Documentation for `BtcProfits`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BtcProfits.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/btc_profits_test.exs
+++ test/btc_profits_test.exs
@@ -1,8 +1,3 @@
 defmodule BtcProfitsTest do
   use ExUnit.Case
-  doctest BtcProfits
-
-  test "greets the world" do
-    assert BtcProfits.hello() == :world
-  end
 end
