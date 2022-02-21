07f9123491dc2df72294675fc98147a4c2937d58
TenTakano <takano20771@gmail.com>
2022-02-21 15:49:08 +0900

    Initial Commit

---
+++ lib/btc_profits.ex
@@ -0,0 +1,18 @@
+defmodule BtcProfits do
+  @moduledoc """
+  Documentation for `BtcProfits`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BtcProfits.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/btc_profits_test.exs
@@ -0,0 +1,8 @@
+defmodule BtcProfitsTest do
+  use ExUnit.Case
+  doctest BtcProfits
+
+  test "greets the world" do
+    assert BtcProfits.hello() == :world
+  end
+end
